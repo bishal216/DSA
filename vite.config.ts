c612a68992b65c65a1a88ae0dcbc35bc9cdee261
bishal216 <bis3hal@gmail.com>
2025-06-02 16:06:15 +0545

    Updates base-url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/DSA/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
