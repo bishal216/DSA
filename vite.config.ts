ba05f6b17792bb9573aafa50f2e6348782969068
bishal216 <bis3hal@gmail.com>
2025-06-02 11:30:07 +0545

    Upload to github pages via vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/DSA/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
