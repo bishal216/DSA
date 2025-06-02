51e20d006ac7a1526137fedff1f613324719887e
bishal216 <bis3hal@gmail.com>
2025-06-02 17:09:57 +0545

    Updates routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,45 +9,51 @@ import Signup from "./pages/signup";
 import PrivateRoute from "./pages/private-route";
 import Dashboard from "./pages/dashboard";
 
-export const router = createBrowserRouter([
+const basename: string = "/DSA";
+export const router = createBrowserRouter(
+  [
+    {
+      path: "",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/search",
+          element: <SearchResults />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/signup",
+          element: <Signup />,
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+        {
+          path: "",
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: "/dashboard",
+              element: <Dashboard />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/search",
-        element: <SearchResults />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-      {
-        path: "",
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: "/dashboard",
-            element: <Dashboard />,
-          },
-        ],
-      },
-    ],
+    basename: basename,
   },
-]);
+);
