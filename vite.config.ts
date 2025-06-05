8a88b9afed2757d257583516421a1dc19be0c0c6
bishal216 <bis3hal@gmail.com>
2025-06-05 13:49:23 +0545

    Update to redirect

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/DSA/",
+  base: "/",
   define: {
     __BASE__: JSON.stringify("/DSA"),
   },
